d99e7f2b279b2a3e34ce19abe7575576103c2271
Hritam06 <hritam.dutta.official@gmail.com>
2022-01-05 20:52:45 +0530

    My first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('I Am Rich'),
+          foregroundColor: Colors.black,
+          backgroundColor: Colors.red,
+        ),
+        backgroundColor: Colors.black,
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/RICH1.gif'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
